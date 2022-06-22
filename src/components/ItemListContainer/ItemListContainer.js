43b53872ebfeedcd58116f72eae6eae116527f45
maxicassarino <maximocassarino@gmail.com>
2022-06-22 00:09:14 -0300

    Desafio N°4

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ItemCount from './ItemCount/ItemCount'
 import './ItemListContainer.css'
 
-let miProd = {id:0, name: "Telstar Ball", price: 5000};
+let miProd = {name: "Telstar Ball", price: 5000};
 let Stock = 1;
 
 const ItemListContainer = (props) => {
     return (
         <div>
             <center>
-                <h6>{props.saludo}</h6>
+                <h2>{props.saludo}</h2>
                 <ItemCount miProd={miProd} Stock={Stock}/>
             </center>
         </div>
